d7c524c8f9466b8d2ec71b55c1ca945bab65f561
JochyDev <joseluisjimenez1998@gmail.com>
2023-09-14 17:18:37 -0400

    [add]; getting better tests

--- test/reservation.test.js
+++ test/reservation.test.js
@@ -1,35 +1,45 @@
 import request from 'supertest';
+import 'dotenv/config';
+
 import { Server } from '../src/server/server.js';
 const server = new Server();
 const app = server.app;
 
+import generateJWT from '../src/helpers/generateJWT.js';
+
+const token = await generateJWT('1');
+
 describe('Reserve a Parking Spot', () => {
     test('POST /api/reservation --> reservation details', async () => {
-        const response = await request(app).post('/api/reservations').send({
-            UserId: '1',
+        const {status, headers, body} = await request(app)
+        .post('/api/reservations')
+        .set('x-token', token)
+        .send({
             startDateTime: new Date(), 
-            startEndTime: new Date() 
-        })
+            endDateTime: new Date() 
+        });
         
-        expect(response.status).toEqual(200);
-        expect(response.headers['content-type']).toEqual(
+        expect(status).toEqual(200);
+        expect(headers['content-type']).toEqual(
             expect.stringContaining('json')
         )
-        expect(response.body.data).toEqual(expect.objectContaining({
+        expect(body.data).toEqual(expect.objectContaining({
             id: expect.any(Number),
             startDateTime: expect.any(String), 
             endDateTime: expect.any(String),
             status: expect.any(String),
             SpotId: expect.any(Number),
-            UserId: expect.any(String)
+            UserId: expect.any(Number)
           }));
     })
 
     test('POST /api/reservations --> startDatetime and endDateTime are requireds!!!', async () => {
-        const response = await request(app).post('api/reservations').send({
-            UserId: '1'
-        })
+        const {status, body} = await request(app)
+        .post('/api/reservations')
+        .set('x-token', token)
+        .send({})
 
-        expect(response.status).toEqual(400)
+        expect(status).toEqual(400),
+        expect(body.data.errors).toBeTruthy();
     })
 })
