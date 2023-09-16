9c47d759f3072ad1a936d08059a7c607b768d023
JochyDev <joseluisjimenez1998@gmail.com>
2023-09-16 06:12:13 -0400

    [add]: adding validate middlewares to Log routes

--- src/routes/log.routes.js
+++ src/routes/log.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { getActivityLogs } from '../controllers/index.js';
+import { validateFields } from '../middlewares/validateFields.js';
+import { validateJWT } from '../middlewares/validateJWT.js';
+import { hasRole } from '../middlewares/validateRoles.js';
 
 const app = Router();
 
-app.get('/', getActivityLogs)
+app.get('/', [
+    validateJWT,
+    hasRole('ADMIN', 'EMPLOYEE'),
+    validateFields
+], getActivityLogs)
 
 
 export default app;
