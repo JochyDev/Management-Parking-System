038b343eac4173fb3dbae23009bc792919d9770d
JochyDev <joseluisjimenez1998@gmail.com>
2023-09-19 20:46:48 -0400

    [update]: db config file from json to js

---
+++ src/config/db.config.js
@@ -0,0 +1,24 @@
+export const config = {
+  "development": {
+    "username": "development",
+    "password": "jljsjljs",
+    "database": "parkingDB",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "development",
+    "password": "jljsjljs",
+    "database": "parkingDB",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+  
