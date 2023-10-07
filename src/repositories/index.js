f9c9e351b3d578aaf91cb665066df2fa6015266b
JochyDev <joseluisjimenez1998@gmail.com>
2023-10-06 21:13:42 -0400

    [add]: spot repository

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,3 +1,4 @@
 export * as userRepository from './user.repository.js'
 export * as logRepository from './log.repository.js'
-export * as reservationRepository from './reservation.repository.js'
+export * as reservationRepository from './reservation.repository.js';
+export * as spotRepository from './spot.repository.js'
---
+++ src/repositories/spot.repository.js
@@ -0,0 +1,13 @@
+import { db } from '../models/sequelize/index.js';
+const { Spot } = db;
+
+export const countSpots = async () => {
+    return await Spot.count()
+} 
+
+
+export const findBySpotNumber = async( spotNumber ) => {
+    return await Spot.findOne({
+        where: { spotNumber }
+      })
+}
